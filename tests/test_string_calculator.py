8017ca0323134de87df524a596d6fcb0c37a6328
verscup <verscup@gmail.com>
2025-03-03 12:55:47 +0100

    test 1 part 3

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -3,6 +3,9 @@ from src.string_calculator import StringCalculator
 
 class TestStringCalculator:
     @pytest.fixture
-    def calculator(self):
+    def stringCalculator(self):
         return StringCalculator()
-   
+
+    def test_empty_string_returns_zero(self, stringCalculator):
+        assert stringCalculator.calculate("") == 0
+
