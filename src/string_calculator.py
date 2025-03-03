4dd22f7ba4867a5f37e9b7b11cf1d8f1562cf3cb
verscup <verscup@gmail.com>
2025-03-03 13:01:59 +0100

    test2

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,8 +1,10 @@
 import re
 
 class StringCalculator:
-    def calculate(self, numbers_str):
+    def calculate(self, numbers_str: str):
         if not numbers_str:
             return 0
         
+        return int(numbers_str)
+    
         
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -9,3 +9,6 @@ class TestStringCalculator:
     def test_empty_string_returns_zero(self, stringCalculator):
         assert stringCalculator.calculate("") == 0
 
+    def test_number_as_input(self, stringCalculator):
+        input_string = "123"    
+        assert stringCalculator.calculate(input_string) == 123
